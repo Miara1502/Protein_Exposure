cef1e67b82eecacfaa2e0be67125792787f28e23
miara1502 <miara.rakotomavo@gmail.com>
2019-09-11 00:50:49 +0200

    debut Extraction coodronnes

--- src/Protein_Exposition.py
+++ src/Protein_Exposition.py
@@ -9,7 +9,7 @@ import numpy as np
 import scipy
 import pandas as pd
 from scipy.spatial import distance_matrix
-from scipy.stats import pearsonr , kendalltau , spearmanr
+
 
 import pprint
 
@@ -25,8 +25,30 @@ def timing(f):
     return wrap
 
 
+visited = {}
+for line in open('3i40.pdb'):
+    list = line.split()
+    id = list[0]
+    if id == 'ATOM':
+        type = list[2]
+        if type == 'CA' or type == 'SG' or type == 'CB':
+            residue = list[3]
+            type_of_chain = list[4]
+            atom_count = int(list[5])
+            position = list[6:9]
+            if atom_count >= 0:
+                if type_of_chain not in visited:
+                    visited[type_of_chain] = 1
+                    print(residue,type_of_chain,atom_count,' '.join(position))
 
 
 
-
-
+'''with open('min.pdb') as pdbfile:
+    for line in pdbfile:
+        if line[:4] == 'ATOM' or line[:6] == "HETATM":
+            print line
+            # Split the line
+            splitted_line = [line[:6], line[6:11], line[12:16], line[17:20], line[21], line[22:26], line[30:38], line[38:46], line[46:54]]
+            print splitted_line
+            # To format again the pdb file with the fields extracted
+            print "%-6s%5s %4s %3s %s%4s    %8s%8s%8s\n"%tuple(splitted_line)'''
