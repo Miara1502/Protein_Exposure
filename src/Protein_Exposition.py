0af4ca782e817aebaefb69334650ed30a3c4c5ac
miara1502 <miara.rakotomavo@gmail.com>
2019-09-17 17:22:55 +0200

    code qui genere des resultats mais qui prend bcp de temps

--- src/Protein_Exposition.py
+++ src/Protein_Exposition.py
@@ -13,8 +13,6 @@ import pandas as pd
 from scipy.spatial import distance_matrix
 import pprint
 
-## Nice wrapper to time functions. Works as a decorator.
-# Taken from https://stackoverflow.com/questions/5478351/python-time-measure-function
 
 def exctraction_coord(pdb_file):
     """Lis un fichier PDB (type = str) et retourne un Pandas DataFrame qui contient
@@ -94,7 +92,7 @@ def calcule_distance(point_sphere , point_atom):
     Il faut rajouter math.sqrt()
     """
     distance = (point_atom['coord_X'] - point_sphere['X'])**2 + (point_atom['coord_Y'] - point_sphere['Y'])**2 + (point_atom['coord_Z'] - point_sphere['Z'])**2
-    return (math.sqrt(distance)) #math.sqrt(dist) pour la racine carré
+    return math.sqrt(distance) #math.sqrt(dist) pour la racine carré
 
 
 def distance_all_point(sphere , atom):
@@ -120,43 +118,14 @@ def distance_all_atom(sphere , coord_dataframe):
     dico_df = pd.DataFrame(dico)
     return dico_df
 
-def ratio_expose(distance_dataframe) :
-    """ Booléen qui return TRUE si la distance entre un point et un atom est
-    inférieur au seuil (cutoff) qui correspond à la somme du diamètre d'une Molécule
-    d'eau et le rayon de l'atom
-    """
-    #dico_rayon = {'H':1.2,'C':1.7,'N':1.55,'O':1.52,'F':1.47,'S':1.8}
-    #TODO : fixer la valeur du cutoff , puis comparer avec les distance de tous les atomes
-    #       Comment déterminer le rayon qu'on ajoute au seuil
-    count = 0
-    for i in range(len(distance_dataframe)):
-        for j in range(len(distance_dataframe.iloc[i])):
-            #print(i, j)
-            if((distance_dataframe.iloc[i][j]) > 4):
-                count = count + 1
-    ratio = count/(distance_dataframe.shape[0]*distance_dataframe.shape[1])
-    return ratio
-
-def surface_sphere(Atom):
-    """Caclul la surface d'un atom
-    """
-    rayon = 0
-    dico_rayon = {'H':1.2,'C':1.8,'N':1.5,'O':1.4,'F':1.47,'S':1.8}
-    for i , (atom, r) in enumerate(dico_rayon.items()):
-        if(Atom['Atom_name'] == atom) :
-            rayon = r
-
-    surface = 4*math.pi*((rayon)**2)
-    return surface
-
-def exposition(ratio , surface):
-    """Permet de calculer la surface exposé au solvant de l'atome
-    """
-    return(ratio*surface)
 
 
 def EXPOSITION(distance_dataframe , Atom):
-
+    """Permet de calculer la surface exposé au solvant de l'atome
+        - Calcul de la surface de l'atom
+        - Calcul du ration exposé
+        - Surface d'exposition : le produit de la surface et le ratio
+    """
     #1 CALCUL DE LA SURFACE
     rayon = 0
     dico_rayon = {'H':1.2,'C':1.8,'N':1.5,'O':1.4,'F':1.47,'S':1.8}
@@ -165,8 +134,36 @@ def EXPOSITION(distance_dataframe , Atom):
             rayon = r
     surface = 4*math.pi*((rayon)**2)
 
-    #2) Calcul du ratio exposé :
-    return surface
+    #print('la surface est : ')
+    #print(surface)
+    #print('le rayon est : ')
+    #print(rayon)
+
+    #2) Calcul du ratio exposé :  #Si DISTANCE > seuil => Point exposé
+    count = 0
+    seuil = 2.8 + rayon
+    for i in range(len(distance_dataframe)):
+        for j in range(len(distance_dataframe.iloc[i])):
+            #print(i, j)
+            if((distance_dataframe.iloc[i][j]) > seuil):
+                count = count + 1
+    ratio = count/(distance_dataframe.shape[0]*distance_dataframe.shape[1])
+
+    #3) Calcul de la surface exposé :
+    return ratio*surface
+
+def protocol(coord_dataframe , nbr_point):
+
+    dico = {}
+    for atom_number in range(len(coord_dataframe)) :
+        atom = coord_dataframe.iloc[atom_number]
+        sphere = translocation(atom , nbr_point)
+        distance_atom = distance_all_atom(sphere , coord_dataframe)
+        expo = EXPOSITION(distance_atom , atom)
+        dico[atom_number] = expo
+
+    return dico
+
 
 
 if __name__ == '__main__' :
@@ -175,33 +172,35 @@ if __name__ == '__main__' :
     print(data)
 
 
-    atom1 = data.iloc[0]
-    atom2 = data.iloc[1]
+    atom1 = data.iloc[0] #atom1
+    atom2 = data.iloc[2] #atom3
 
     ##################################EXEMPLE POUR UNE SPHERE/ ATOM #######################
     #translocation pour l'atome 01
-    sphere_atom1 = translocation(atom1, 100) #N: Nuage de points de la sphère
+    sphere_atom1 = translocation(atom1, 10) #N: Nuage de points de la sphère
 
-    res = distance_all_point(sphere_atom1 , atom2)
-    print('la distance entre la sphere 1 et l atom2 est : \n')
-    #print(res)
+    Matrix_atom1 = distance_all_atom(sphere_atom1 , data)
+    #print(Matrix_atom1)
 
-    resultat = distance_all_atom(sphere_atom1 , data)
-    #print(resultat)
-    #print(resultat.iloc[1][0])
+    expo1 = EXPOSITION(Matrix_atom1, atom1)
+    print('exposition de l atom 0 : ')
+    print(expo1)
 
-    print(resultat)
-    ratio = ratio_expose(resultat)
-    print(ratio)
+    print('\n')
+    sphere_atom2 = translocation(atom2, 10) #N: Nuage de points de la sphère
 
-    test = surface_sphere(atom1)
-    print(test)
+    Matrix_atom2 = distance_all_atom(sphere_atom2 , data)
+    #print(Matrix_atom2)
 
-    exp = exposition(ratio , test)
-    print(exp)
+    expo2 = EXPOSITION(Matrix_atom2, atom2)
+    print('exposition de l atom 2 : ')
+    print(expo2)
 
 
     ########################################################################################
+    #test sur tous les atoms :
+    A = protocol(data , 10)
+    print(A)
 
 
 
