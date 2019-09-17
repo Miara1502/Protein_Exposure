f2170e8e67b6970edff0fd2fa39d78d7167ed7c2
miara1502 <miara.rakotomavo@gmail.com>
2019-09-17 18:29:52 +0200

    ajout des fichiers pour tester le programme

--- src/brouillon.py
+++ src/brouillon.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8  -*-
-
-"""Module with all the function that we need for Surface.solvant.py.
-"""
-
 import math
 import time
 import numpy as np
@@ -12,66 +7,89 @@ import pandas as pd
 from scipy.spatial import distance_matrix
 import pprint
 
-## Nice wrapper to time functions. Works as a decorator.
-# Taken from https://stackoverflow.com/questions/5478351/python-time-measure-function
-def timing(f):
+def exctraction_coord(pdb_file):
+    """Lis un fichier PDB (type = str) et retourne un Pandas DataFrame qui contient
+    les coordonnées de chaque atome , leurs nom ainsi que leur résidus
 
-    """Nice wrapper to time functions. Works as a decorator.
-    Taken from https://stackoverflow.com/questions/5478351/python-time-measure-function
     """
 
-    def wrap(*args):
-        time1 = time.time()
-        ret = f(*args)
-        time2 = time.time()
-        print('{:s} function took {:.3f} ms'.format(f.__name__, (time2-time1)*1000.0))
-        return ret
-    return wrap
+    filin = open(pdb_file)
+    #position_file.seek(0)
 
-atom_coord = {"atom_central" : [] , "residu" : [] , "coord_X" : [] ,
-             "coord_Y" : [] , "coord_Z" : []}
-list_atom = []
-list_residu = []
-X = []
-Y = []
-Z = []
-for line in open('3i40.pdb'):
-    list = line.split()
-    id = list[0]
-    if id == 'ATOM':
-        list_atom.append(list[2])
-        list_residu.append(list[3])
-        X.append(float(list[6]))
-        Y.append(float(list[7]))
-        Z.append(float(list[8]))
+    atom_dico = {"atom_central" : [] , "residu" : [] , "coord_X" : [] ,
+                 "coord_Y" : [] , "coord_Z" : []}
+    list_atom = []
+    list_residu = []
+    X = []
+    Y = []
+    Z = []
+    for line in filin:
+        list = line.split()
+        id = list[0]
+        if id == 'ATOM':
+            list_atom.append(list[2])
+            list_residu.append(list[3])
+            X.append(float(list[6]))
+            Y.append(float(list[7]))
+            Z.append(float(list[8]))
+    atom_dico['atom_central'] = list_atom
+    atom_dico['residu'] = list_residu
+    atom_dico['coord_X'] = X
+    atom_dico['coord_Y'] = Y
+    atom_dico['coord_Z'] = Z
 
-atom_coord['atom_central'] = list_atom
-atom_coord['residu'] = list_residu
-atom_coord['coord_X'] = X
-atom_coord['coord_Y'] = Y
-atom_coord['coord_Z'] = Z
+    coord_dataframe = pd.DataFrame(atom_dico)
+    coord_dataframe['Atom_name'] = coord_dataframe['atom_central'].astype(str).str[0]
 
-data = pd.DataFrame(atom_coord)
+    return coord_dataframe
 
 
-import numpy
+def golden_sphere(n):
+    """Crée un nuage de point dstribué dans une sphère / Algortihm Golden Spirale
+    """
+    golden_angle = np.pi * (3 - np.sqrt(5))
+    theta = golden_angle * np.arange(n)
+    z = np.linspace(1 - 1.0 / n, 1.0 / n - 1, n)
+    radius = np.sqrt(1 - z * z)
 
-n = 100
+    points = np.zeros((n, 3))
+    points[:,0] = radius * np.cos(theta)
+    points[:,1] = radius * np.sin(theta)
+    points[:,2] = z
+    sphere = pd.DataFrame(points)
+    sphere.columns = ['X' , 'Y' , 'Z']
 
-golden_angle = numpy.pi * (3 - numpy.sqrt(5))
-theta = golden_angle * numpy.arange(n)
-z = numpy.linspace(1 - 1.0 / n, 1.0 / n - 1, n)
-print(z)
-radius = numpy.sqrt(1 - z * z)
-
-points = numpy.zeros((n, 3))
-points[:,0] = radius * numpy.cos(theta)
-points[:,1] = radius * numpy.sin(theta)
-points[:,2] = z
-
-#translocations :
-#Generer une sphère1 :
-sphere1 = golden_sphere(100)
+    return sphere
 
 
-#
+def translocation(Atom , n):
+    """Translocation d'une sphere pour un atom en utilisant l'atome comme origine de la sphère (1)
+        - Créer une sphère avec la fonction Golden_sphere
+        - Créer un dictionnaire contenant le radius de chaque atome dans le fichier PDB
+        - Translocation de la sphère
+        - N : Correspond au nombre de nuage de points qu'on souhaite générer
+    """
+    nouvelle_sphere = golden_sphere(n)
+    #print(sphere)
+
+    dico_rayon = {'H':1.2,'C':1.7,'N':1.55,'O':1.52,'F':1.47,'S':1.8}
+    for i , (atom, r) in enumerate(dico_rayon.items()):
+        if(Atom['Atom_name'] == atom) :
+            rayon = r
+            #print(rayon)
+            nouvelle_sphere['X'] = (nouvelle_sphere['X'] + Atom['coord_X'])*rayon
+            nouvelle_sphere['Y'] = (nouvelle_sphere['Y'] + Atom['coord_Y'])*rayon
+            nouvelle_sphere['Z'] = (nouvelle_sphere['Z'] + Atom['coord_Z'])*rayon
+
+    return nouvelle_sphere
+
+
+
+
+if __name__ == '__main__':
+    data = exctraction_coord('CD59_2J8B.pdb')
+    print(data)
+
+
+    df = pd.DataFrame(dico)
+    print(df)
